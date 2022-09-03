77e52d70d46734b5651cf2ee56a6eb244bae999e
tradennn1 <treidnnn@gmail.com>
2022-09-04 03:14:33 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double rand = Math.random();
+        {
+
+            System.out.println("Random Number:" + rand);
+
+        }
+        System.out.println(Student(29, 48));
+        System.out.println(Student(60, 30));
+        System.out.println(Student(80, 38));
+        System.out.println(Student(28, 20));
+        System.out.println(Student(24, 36));
+
+    }
+
+    private static String Student(int age, int temp) {
+        String walk = ("Можно идти гулять");
+        String stayHome = ("Оставайтесь дома");
+        if ((age > 20 && age < 45) && (temp > -20 && temp < 30)) {
+            return walk;
+        } else if ((age < 20) && (temp >= 0 && temp < 28)) {
+            return walk;
+        } else if ((age > 45) && (temp > -10 && temp < 25)) {
+            return walk;
+        } else {
+            return stayHome;
+        }
     }
 }
